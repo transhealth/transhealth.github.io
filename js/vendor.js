dc87571936d2cfb1e0d605f68eb509edefd44922
xles <xles@mirakulix.org>
2016-04-18 23:08:32 +0200

    Playing around with layout and colour scheme...

--- js/vendor.js
+++ js/vendor.js
@@ -1,6 +1,7 @@
 ---
 ---
 
+{% include bower_components/prefixfree/prefixfree.js %}
 {% include bower_components/jquery/dist/jquery.min.js %}
 {% include bower_components/foundation-sites/dist/foundation.min.js %}
 
